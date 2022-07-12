6ba02b68badfef8f58a7df64934152957fb2c6f2
jultty <juffff@tutanota.de>
2022-07-11 23:31:03 -0300

    Retira código não-utilizado

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,6 @@ const botaoMenu = document.getElementById("nav-botao");
 
 $("#nav-botao").click(function(){
     $("#nav-menu").fadeToggle("slow");
-    // botaoMenu.style.marginBottom = "40px";
     $("#nav-botao").toggleClass("nav-botao-fundo");
     $("#nav-botao").toggleClass("nav-botao-topo");
 }); 
