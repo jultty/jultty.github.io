e635a780be7d4cf491a77dba550752fe2ba3e701
jultty <juffff@tutanota.de>
2022-11-19 14:38:12 -0300

    Implementa a troca de idiomas por notificação

---
+++ assets/js/lang.js
@@ -0,0 +1,11 @@
+const langNotification = document.getElementById("lang-notification");
+const userLang = navigator.language || navigator.userLanguage;
+const pageLang = document.getElementsByTagName('html')[0].getAttribute('lang');
+
+if (userLang != pageLang) {
+    $("#lang-notification").fadeIn("slow");    
+}
+
+$("#lang-dismiss").click(function(){
+    $("#lang-notification").fadeOut("fast");
+});
